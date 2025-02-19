f99cc5840eb45d1d09cc655bc3346e9b64db1f2a
Ivan <ovchinno.ivan@gmail.com>
2025-02-19 11:45:22 +0300

    [DEV] - Create app realization

--- main.cpp
+++ main.cpp
@@ -1,39 +1,7 @@
-#include "planet/planet.hpp"
+#include "src/app.hpp"
 
-#include <cstring>
-#include <iostream>
-
-int main() {
-    const int BUFFER_SIZE = 256;
-    const char* filename = "source.txt";
-
-    planet::Planet** solarSystem = new planet::Planet*[BUFFER_SIZE];
-    int size = 0;
-    planet::Planet::readFromFile(filename, solarSystem, size);
-
-    std::cout << std::endl << "Before sorting" << std::endl;
-    planet::Planet::print(solarSystem, size);
-    planet::Planet::sort(solarSystem, size);
-    std::cout << std::endl << "After sorting" << std::endl;
-    planet::Planet::print(solarSystem, size);
-
-    char* plutoName = new char[6];
-    std::strcpy(plutoName, "Pluno");
-    planet::Planet* pluto = new planet::Planet(plutoName, 2280, 0, 1);
-    delete plutoName;
-    std::cout << std::endl << *pluto << " - appending to solar system" << std::endl << std::endl;
-    planet::Planet::append(solarSystem, size, pluto);
-    planet::Planet::print(solarSystem, size);
-    planet::Planet* removePlanet = solarSystem[std::min(size, 5)];
-    std::cout << std::endl << *removePlanet << " - removing from solar system" << std::endl << std::endl;
-    planet::Planet::remove(solarSystem, size, removePlanet);
-    planet::Planet::print(solarSystem, size);
-
-    const char* fileout = "output.txt";
-    std::cout << "Write changes to file " << fileout << std::endl;
-    planet::Planet::writeToFile(fileout, solarSystem, size);
-
-    delete[] solarSystem;
+int main(int argc, char** argv) {
+    app::run(argc, argv);
 
     return 0;
 }
---
+++ src/app.cpp
@@ -0,0 +1,158 @@
+#include "app.hpp"
+#include "../house/house.hpp"
+#include "../planet/planet.hpp"
+
+#include <cstring>
+#include <iostream>
+
+namespace {
+
+const int BUFFER_SIZE = 256;
+const int defaultSize = 0;
+
+bool isValidInteractiveType(int type) {
+    return type > 0 && type < 8;
+}
+
+}  // namespace
+
+namespace app {
+
+ProgramMode getProgramMode(int argc, char** argv) {
+    if (argc == 2 && std::strcmp(argv[1], "-it") == 0) {
+        return ProgramMode::interactive;
+    }
+
+    return ProgramMode::demonstrative;
+}
+
+void runDemonstrativeMode() {
+    const char* filename = "source.txt";
+
+    planet::Planet** solarSystem = new planet::Planet*[BUFFER_SIZE];
+    int size = 0;
+    planet::Planet::readFromFile(filename, solarSystem, size);
+
+    std::cout << std::endl << "Before sorting" << std::endl;
+    planet::Planet::print(solarSystem, size);
+    planet::Planet::sort(solarSystem, size);
+    std::cout << std::endl << "After sorting" << std::endl;
+    planet::Planet::print(solarSystem, size);
+
+    char* plutoName = new char[6];
+    std::strcpy(plutoName, "Pluno");
+    planet::Planet* pluto = new planet::Planet(plutoName, 2280, 0, 1);
+    delete plutoName;
+    std::cout << std::endl << *pluto << " - appending to solar system" << std::endl << std::endl;
+    planet::Planet::append(solarSystem, size, pluto);
+    planet::Planet::print(solarSystem, size);
+    planet::Planet* removePlanet = solarSystem[std::min(size, 5)];
+    std::cout << std::endl << *removePlanet << " - removing from solar system" << std::endl << std::endl;
+    planet::Planet::remove(solarSystem, size, removePlanet);
+    planet::Planet::print(solarSystem, size);
+
+    const char* fileout = "output.txt";
+    std::cout << "Write changes to file " << fileout << std::endl;
+    planet::Planet::writeToFile(fileout, solarSystem, size);
+
+    delete[] solarSystem;
+}
+
+InteractiveType getInteractiveType() {
+    std::cout << "print - 1; read from file - 2; write to file - 3; append element - 4; remove element - 5; sort - 6; exit - 7" << std::endl;
+
+    int type{};
+    std::cin >> type;
+
+    if (!isValidInteractiveType(type)) {
+        std::cout << "Invalid input!" << std::endl;
+        return getInteractiveType();
+    }
+
+    return static_cast<InteractiveType>(type);
+}
+
+void runInteractiveMode() {
+    bool running = true;
+    int size = defaultSize;
+    house::House** housingDepartament = new house::House*[size];
+
+    while (running) {
+        switch (getInteractiveType()) {
+            case InteractiveType::print: {
+                if (size == 0) {
+                    std::cout << "empty" << std::endl;
+                } else {
+                    house::House::print(housingDepartament, size);
+                }
+                break;
+            }
+            case InteractiveType::read: {
+                char* filename = new char[BUFFER_SIZE];
+                std::cout << "Enter filename: ";
+                std::cin >> filename;
+                house::House::readFromFile(filename, housingDepartament, size);
+                std::cout << "Data read from file " << filename << std::endl;
+                delete[] filename;
+                break;
+            }
+            case InteractiveType::write: {
+                char* filename = new char[BUFFER_SIZE];
+                std::cout << "Enter filename: ";
+                std::cin >> filename;
+                house::House::writeToFile(filename, housingDepartament, size);
+                std::cout << "Data wrote to file " << filename << std::endl;
+                delete[] filename;
+                break;
+            }
+            case InteractiveType::append: {
+                std::cout << "Enter new element (street number floors apartaments)" << std::endl;
+                char* street = new char[BUFFER_SIZE];
+                int number{}, countOfFloors{}, countOfApartaments{};
+                std::cin >> street >> number >> countOfFloors >> countOfApartaments;
+                house::House* newElement = new house::House(street, number, countOfFloors, countOfApartaments);
+                delete[] street;
+                house::House::append(housingDepartament, size, newElement);
+                std::cout << *newElement << "added to db" << std::endl;
+                break;
+            }
+            case InteractiveType::remove: {
+                std::cout << "Enter element to remove (street number floors apartaments)" << std::endl;
+                char* street = new char[BUFFER_SIZE];
+                int number{}, countOfFloors{}, countOfApartaments{};
+                std::cin >> street >> number >> countOfFloors >> countOfApartaments;
+                house::House* removeElement = new house::House(street, number, countOfFloors, countOfApartaments);
+                delete[] street;
+                house::House::remove(housingDepartament, size, removeElement);
+                std::cout << *removeElement << "removed from db" << std::endl;
+                break;
+            }
+            case InteractiveType::sort: {
+                house::House::sort(housingDepartament, size);
+                std::cout << "Data was sorted" << std::endl;
+                break;
+            }
+            case InteractiveType::exit: {
+                std::cout << "Goodbuy!" << std::endl;
+                delete[] housingDepartament;
+                running = false;
+                break;
+            }
+        }
+    }
+}
+
+void run(int argc, char** argv) {
+    switch (getProgramMode(argc, argv)) {
+        case ProgramMode::interactive: {
+            std::cout << "Welcome!" << std::endl;
+            runInteractiveMode();
+            break;
+        }
+        case ProgramMode::demonstrative:
+            runDemonstrativeMode();
+            break;
+    }
+}
+
+}  // namespace app
